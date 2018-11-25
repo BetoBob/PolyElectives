812b67f81d73029374c254616424a176ffb28b6a
pmkramer <kramerpatrick48@gmail.com>
2018-11-24 19:11:03 -0800

    0015: AboutTest

--- src/main/java/test/AboutTest.java
+++ src/main/java/test/AboutTest.java
@@ -3,6 +3,7 @@ package test;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 import org.junit.Test;
 
@@ -10,50 +11,24 @@ import logic.About;
 import logic.Account;
 import logic.Home;
 import logic.Page;
+import logic.PolyElectives;
 import javafx.stage.Stage;
 import logic.Quiz;
 import logic.Tutorial;
 
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
-public class AboutTest extends ApplicationTest{
+public class AboutTest extends FxRobot {
 
-	
-	@Override
-    public void start(Stage stage) throws IOException {
-		stage.setTitle("PolyElectives");
-		
-		// Create all the pages
-		Page about = new About();
-		Page home = new Home();
-		Page tutorial = new Tutorial();
-		Page quiz = new Quiz();
-		Page account = new Account();
-		
-		Page[] pages = {about, home, tutorial, quiz, account};
-		
-		// Set up menu links between pages
-		for (Page p: pages)
-		{
-			p.getMenu().setUpNavigation(stage, pages);
-		}
-		
-		// Set up home links
-		((Home)home).setUpNavigation(stage, pages);
-		
-		// Set up first page to view
-		home.getMenu().setCurrentPage("home");
-		home.getMenu().highlightPage("home");
-		stage.setScene(pages[Home.ID_PAGE].getScene());
-		
-		stage.show();
-    }
 
     @Test
     public void title() throws Exception {
+    	FxToolkit.registerPrimaryStage();
+    	FxToolkit.setupApplication(PolyElectives.class);
     	rightClickOn("#about");
     	assertNotNull("#about");
-    	stop();
     }
     
 }
