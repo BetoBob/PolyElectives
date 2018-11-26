70a5433881c845398f10a56c9e56337d12c4490b
jojopak <pak.j.jedi@gmail.com>
2018-11-26 14:32:08 -0800

    placeholders in Tutorial

--- src/main/java/logic/Tutorial.java
+++ src/main/java/logic/Tutorial.java
@@ -1,7 +1,16 @@
 package logic;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class Tutorial extends Base implements Page {
 	
@@ -16,12 +25,42 @@ public class Tutorial extends Base implements Page {
 	public void renderPage() {
 		root = new VBox();
 		VBox subPage = createSub(root);
+		HBox imgs = new HBox();
 		Background b = new Background();
 		menu = new Menu();
 		b.add(menu.getRoot());
 		b.add(subPage);
 		root.getChildren().addAll(b.getRoot());
 		scene = new Scene(root, 1200, 800);
+		
+		Label title = new Label("Tutorial");
+		title.setId("title");
+		title.setMinWidth(300);
+		title.setMinHeight(210);
+		title.setTextFill(Color.BLACK);
+		title.setAlignment(Pos.CENTER);
+		title.setFont(Font.font("Tahoma", FontWeight.BOLD, 40));
+		
+		Label step1 = new Label("Step 1:");
+		step1.setMinWidth(500);
+		step1.setMinHeight(75);
+		step1.setTextFill(Color.BLACK);
+		step1.setStyle("-fx-background-color: #B5A76C;");
+		subPage.getAlignment();
+		step1.setAlignment(Pos.CENTER);
+		step1.setFont(Font.font("Tahoma", FontWeight.THIN, 40));
+		
+		Region spacer1 = new Region();
+		spacer1.setPrefHeight(60);
+		
+		ImageView step_placeholder = new ImageView(new Image(getClass().getResourceAsStream("placeholder.png"), 1000, 200, true, true));
+		imgs.setAlignment(Pos.TOP_LEFT);
+		imgs.getChildren().addAll(step_placeholder);
+		
+		subPage.getChildren().addAll(title,step1,spacer1,step_placeholder);
+		//subPage.getChildren().addAll(title,step1);
+		
+
 	}
 	
 	@Override
