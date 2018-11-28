9cab4f783590f298524068127ddfb90e558cd3f0
pmkramer <kramerpatrick48@gmail.com>
2018-11-27 18:44:28 -0800

    0023: QuizTest

--- src/main/java/test/QuizTest.java
+++ src/main/java/test/QuizTest.java
@@ -10,12 +10,18 @@ import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import logic.Quiz;
 import logic.QuizQuestion;
 
 public class QuizTest extends ApplicationTest{
 
-	// Patrcik Kramer unit test
+	@Override
+	public void start(Stage s) {
+		return;
+	}
+	
+	// Patrick Kramer unit test
 		@Test
 		public void testButtons() throws IOException {
 			Quiz q = new Quiz();
